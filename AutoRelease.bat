233f59b5ec40a3fb2738ad40c571b7163c4a98f3
stackprobe <stackprobes@gmail.com>
2020-04-30 02:30:01 +0900

    Cosmo

--- AutoRelease.bat
+++ AutoRelease.bat
@@ -1,26 +1,22 @@
-IF NOT EXIST WPrime64\. GOTO END
 CLS
 rem リリースして qrum します。
 PAUSE
 
+CALL newcsrr
+
 CALL ff
 cx **
-
 CD /D %~dp0.
-
-IF NOT EXIST WPrime64\. GOTO END
 
 CALL qq
 cx **
 
 CALL _Release.bat /-P
 
-MOVE out\Prime64.zip S:\リリース物\.
+MOVE out\* S:\リリース物\.
 
 START "" /B /WAIT /DC:\home\bat syncRev
 
 CALL qrumauto rel
 
 rem **** AUTO RELEASE COMPLETED ****
-
-:END
